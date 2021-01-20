b90d08c5e1f1647a2833b6c899aee3afd3941d6b
Kanyarut Pornamnuay <benbaba2525@hotmail.com>
2021-01-19 21:42:41 -0800

    Created user file and added signup.js

--- src/MainRouter.js
+++ src/MainRouter.js
@@ -1,10 +1,14 @@
 import React from "react";
-//import { Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Home from "./core/Home";
+import Signup from "./user/Signup"
 
 const MainRouter = () => (
   <div>
-     <Home />
+     <Switch>
+        <Route path="/" component={Home}></Route>
+        <Route path="/signup" component={Signup}></Route>
+     </Switch>
 
   </div>
 
