0cac21a076e307e5bcef45599b50494f790d1483
Kanyarut Pornamnuay <benbaba2525@hotmail.com>
2021-02-02 19:12:22 -0800

    Uninstall express-validator and install install the older version: npm install express-validator@5.3.1 inorder to make it work

--- server/app.js
+++ server/app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
-const expressValidator = require('express-validator');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
 
 
 const config = require('./config/config');
