fd2ab07f3aaed15e1cf5de1eabc595a58485c451
Kanyarut Pornamnuay <benbaba2525@hotmail.com>
2021-02-12 21:39:53 -0800

    Installed JWT and cookie parser

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const expressValidator = require('express-validator');
 
 
